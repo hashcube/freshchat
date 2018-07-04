78ab8c5027668465e3d3cc40701e602cb440dd3e
Alex Shcherbyna <alex@Alexs-MacBook-Pro.local>
2018-07-04 23:54:29 +0300

    copy strings file to new project path, issue #26

--- build/index.js
+++ build/index.js
@@ -12,7 +12,12 @@ exports.onCreateProject = function (api, app, config, cb) {
   if (config.target == 'native-android') {
     xmlStr = new xmldoc.XmlDocument(fs.readFileSync(path.join(__dirname, '../android/strings.xml'), 'utf-8'));
     xmlStr.children[0].val = packageName + ".provider";
-    return fs.outputFileAsync(path.join(androidProjectPath, 'res/values', strings_file), xmlStr.toString(), 'utf-8');
+      return fs.outputFileAsync(path.join(androidProjectPath,'../../',
+          app.manifest.shortName,
+          "tealeaf/src/main",
+          'res/values',
+          strings_file),
+          xmlStr.toString(), 'utf-8');
   }
 
   return Promise.resolve(true);
